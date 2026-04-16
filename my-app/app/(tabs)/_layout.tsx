f0f65c2e81f9199f268ceb9c755ba5ad8cced340
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-04-17 00:43:39 +0530

    fix: adjust TabIcon layout and styling for improved appearance

--- my-app/app/(tabs)/_layout.tsx
+++ my-app/app/(tabs)/_layout.tsx
@@ -9,19 +9,18 @@ const TabIcon = ({focused,icon,title}:any) => {
       return(
           <ImageBackground
               source={images.highlight}
-              className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-2 justify-center items-center rounded-full overflow-hidden"
+              className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
           >
               <Image source={icon} tintColor="#151312" className="size-5"/>
               <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
           </ImageBackground>
       )
-  }else{
+  }
       return (
           <View className="size-full justify-center items-center mt-4 rounded-full">
               <Image source={icon} tintColor="#A8B5DB" className="size-5" />
           </View>
       )
-  }
 }
 
 const _Layout = () => {
@@ -30,15 +29,15 @@ const _Layout = () => {
         screenOptions={{
             tabBarShowLabel:false,
             tabBarItemStyle: {
-                width:'100%',
-                height:'100%',
+                width:"100%",
+                height:"100%",
                 justifyContent:'center',
                 alignItems:'center'
             },
             tabBarStyle:{
                 backgroundColor:'#0f0D23',
                 borderRadius:50,
-                marginHorizontal:10,
+                marginHorizontal:20,
                 marginBottom:36,
                 height:52,
                 position:'absolute',
