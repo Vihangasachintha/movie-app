3562cf3165e2d8b086adb240e6ac6f02242ddd0e
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-04-17 13:03:54 +0530

    feat: implement SearchBar component with routing and placeholder support

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import {Text, View, Image, ScrollView} from "react-native";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import { useRouter} from "expo-router";
 
 export default function Index() {
+    const router = useRouter();
     return (
         <View className="flex-1 bg-primary"
         >
@@ -15,7 +17,10 @@ export default function Index() {
                 <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
 
                 <View className="flex-1 mt-5">
-                    <SearchBar  />
+                    <SearchBar
+                    onPress={()=>router.push("/search")}
+                    placeholder="Search for a movie"
+                    />
                 </View>
             </ScrollView>
 
