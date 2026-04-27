5d4b10aa7c1333ca65c5fd1b22a9885a25ecbfbd
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-04-27 11:20:51 +0530

    feat: integrate trending movies functionality and improve error handling in Index component

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -6,9 +6,16 @@ import {useRouter} from "expo-router";
 import useFetch from "@/services/useFetch";
 import {fetchMovies} from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import {getTrendingMovies} from "@/services/appwrite";
 
 export default function Index() {
     const router = useRouter();
+
+    const {
+        data: trendingMovies,
+        loading: trendingLoading,
+        error: trendingError
+    } = useFetch(getTrendingMovies);
 
     const {
         data: movies,
@@ -28,41 +35,61 @@ export default function Index() {
             }}>
                 <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
 
-                {movieLoading ? (
+                {movieLoading || trendingLoading ? (
                     <ActivityIndicator
                         size="large"
                         color="#0000ff"
                         className="mt-10 self-center"
                     />
-                ) : movieError ? (
-                    <Text>Error: {movieError?.message}</Text>
+                ) : movieError || trendingError ? (
+                    <Text>Error: {movieError?.message || trendingError?.message}</Text>
                 ) : (
                     <View className="flex-1 mt-5">
                         <SearchBar
                             onPress={() => router.push("/search")}
-                            placeholder="Search for a movie"
-                        />
+                            placeholder="Search for a movie" value={""} onChangeText={function (text: string): void {
+                            throw new Error("Function not implemented.");
+                        }}/>
+
+                        {
+                            trendingMovies && (
+                                <View className="mt-10">
+                                    <Text className="text-lg text-white font-bold mb-3">Trending Movies</Text>
+                                </View>
+                            )
+                        }
 
                         <>
+                            <FlatList
+                                      horizontal
+                                      showsHorizontalScrollIndicator={false}
+                                      ItemSeparatorComponent={()=> <View className="w-4"/>}
+                                      className="mb-4 mt-3"
+                                      data={trendingMovies}
+                                      renderItem={({item, index}) => (
+                                          <Text className="text-white text-sm">{item.title}</Text>
+                                      )}
+                                      keyExtractor={(item) => item.movie_id.toString()}
+                            >
+                            </FlatList>
+
                             <Text className="text-lg text-white font-bold mt-5 mb-3">
                                 Latest Movies
                             </Text>
+
                             <FlatList data={movies}
-                            renderItem={({item})=>(
-                                // <Text className="text-white text-sm">
-                                //     {item.title}
-                                // </Text>
-                                <MovieCard
-                                    {...item}
-                                />
-                            )}
-                            keyExtractor={(item)=>item.id.toString()}
+                                      renderItem={({item}) => (
+                                          <MovieCard
+                                              {...item}
+                                          />
+                                      )}
+                                      keyExtractor={(item) => item.id.toString()}
                                       numColumns={3}
                                       columnWrapperStyle={{
-                                          justifyContent:'flex-start',
-                                          gap:20,
-                                          paddingRight:5,
-                                          marginBottom:10
+                                          justifyContent: 'flex-start',
+                                          gap: 20,
+                                          paddingRight: 5,
+                                          marginBottom: 10
                                       }}
                                       className="mt-2 pb-32"
                                       scrollEnabled={false}
