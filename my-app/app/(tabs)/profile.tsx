c6ed1e393b5f24fa77019ee88956f540e8bcb16a
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-04-29 21:35:31 +0530

    feat: enhance Profile component with SafeAreaView and improved layout

--- my-app/app/(tabs)/profile.tsx
+++ my-app/app/(tabs)/profile.tsx
@@ -1,11 +1,17 @@
 import {StyleSheet,View, Text } from 'react-native';
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image } from 'expo-image';
+import { icons } from '@/constants/icons';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+      <SafeAreaView className="bg-primary flex-1 px-10">
+          <View className="flex justify-center items-center flex-1 flex-col gap-5">
+              <Image source={icons.person} className="size-10" tintColor="#fff" />
+              <Text className="text-gray-500 text-base">Profile</Text>
+          </View>
+      </SafeAreaView>
   );
 };
 
