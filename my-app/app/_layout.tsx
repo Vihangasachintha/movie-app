a8be76854d4108d30f2d9bad73328bad71ae08ed
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-04-29 21:10:12 +0530

    feat: enhance RootLayout component with StatusBar visibility control

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -1,12 +1,17 @@
-import { Stack } from "expo-router";
+import {Stack} from "expo-router";
 import './global.css';
+import {StatusBar} from "expo-status-bar";
 
 export default function RootLayout() {
-  return (
-      <Stack>
-        <Stack.Screen name="(tabs)"
-        options={{
-          headerShown:false,
-        }}/>
-      </Stack>);
+    return (
+        <>
+            <StatusBar hidden={true} />
+            <Stack>
+                <Stack.Screen name="(tabs)"
+                              options={{
+                                  headerShown: false,
+                              }}/>
+            </Stack>
+        </>
+    );
 }
