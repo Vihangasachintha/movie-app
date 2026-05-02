97bcd3b78ac3f7fc09a64356609f8c1d0b31f9f8
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-05-02 12:00:21 +0530

    feat: add movieSampleCard image to images export

--- my-app/constants/images.ts
+++ my-app/constants/images.ts
@@ -1,9 +1,11 @@
 import bg from "@/assets/images/bg.png";
 import highlight from "@/assets/images/highlight.png";
 import rankingGradient from "@/assets/images/rankingGradient.png";
+import movieSampleCard from "@/assets/images/movie_sample_card.png";
 
 export const images = {
   bg,
   highlight,
   rankingGradient,
+  movieSampleCard,
 };
