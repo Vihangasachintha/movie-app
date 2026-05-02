b5143bed6a3c577b2774129dcf14696899756288
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-05-02 10:18:24 +0530

    feat: refactor Profile component layout and enhance UI elements

--- my-app/app/(tabs)/profile.tsx
+++ my-app/app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet,View, Text, Image } from 'react-native';
+import {View, Text, Image, TouchableOpacity } from 'react-native';
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { icons } from '@/constants/icons';
@@ -6,13 +6,56 @@ import { icons } from '@/constants/icons';
 const Profile = () => {
   return (
       <SafeAreaView className="bg-primary flex-1 px-10">
-          <View className="flex justify-center items-center flex-1 flex-col gap-5">
-              <Image source={icons.person} className="size-10" tintColor="#fff" />
-              <Text className="text-gray-500 text-base">Profile</Text>
+          <View className="flex-row items-center justify-between px-5 py-3">
+
+              <View className="flex-row items-center gap-2">
+                  <Image
+                      source={icons.logo}
+                      className="w-5 h-5"
+                      resizeMode="cover"
+                  />
+                  <Text className="text-accent font-bold text-lg">MovieHub</Text>
+              </View>
+
+              <TouchableOpacity>
+                  <Image
+                      source={icons.config}
+                      className="w-6 h-6"
+                      tintColor="#fff"
+                      resizeMode="contain"
+                  />
+              </TouchableOpacity>
+
           </View>
+
+          <View className="flex-col items-center justify-center">
+              <View className="flex-col items-center gap-2">
+                  <View className="border-2 p-3 rounded-full">
+                      <Image
+                          source={icons.user}
+                          className="w-20 h-20"
+                          tintColor="#fff"
+                          resizeMode="contain"
+                      />
+                  </View>
+                  <View className="absolute bottom-5 right-0 rounded-full p-1">
+                      <Image
+                          source={icons.pen}
+                          className="w-5 h-5 rounded-full"
+                          resizeMode="contain"
+                      />
+                  </View>
+              </View>
+              <View>
+                  <Text className="text-white text-lg font-bold">John Doe</Text>
+              </View>
+              <View>
+                  <Text className="text-white text-sm">johndoe@moviehub</Text>
+              </View>
+          </View>
+
       </SafeAreaView>
   );
 };
 
 export default Profile;
-const styles = StyleSheet.create({})
